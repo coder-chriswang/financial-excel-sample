a1430926942135f74363832c90d7b0867c2d9c67
王超 <01482445@yto.net.cn>
2019-03-27 10:54:18 +0800

    update:去掉废弃的方法

--- src/main/java/net/yto/sample/ytopermissionsample/YtoPermissionSampleApplication.java
+++ src/main/java/net/yto/sample/ytopermissionsample/YtoPermissionSampleApplication.java
@@ -1,9 +1,8 @@
 package net.yto.sample.ytopermissionsample;
 
 import cn.hutool.core.bean.BeanUtil;
-import io.swagger.annotations.ApiResponse;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -137,163 +136,163 @@ public class YtoPermissionSampleApplication {
                     ExcelData excelData = new ExcelData();
                     // 获取发票种类
                     if (row.getCell(0) != null) {
-                        row.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(0).setCellType(CellType.STRING);
                         String invoiceType = row.getCell(0).getStringCellValue();
                         excelData.setInvoiceType(invoiceType);
                     }
                     // 单据号
                     if (row.getCell(1) != null) {
-                        row.getCell(1).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(1).setCellType(CellType.STRING);
                         String invoiceNum = row.getCell(1).getStringCellValue();
                         excelData.setInvoiceNum(invoiceNum);
                     }
                     // 单据日期
                     if (row.getCell(2) != null) {
-                        row.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(2).setCellType(CellType.STRING);
                         String time = row.getCell(2).getStringCellValue();
                         excelData.setTime(time);
                     }
                     // 客户编码
                     if (row.getCell(3) != null) {
-                        row.getCell(3).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(3).setCellType(CellType.STRING);
                         String customerCode = row.getCell(3).getStringCellValue();
                         excelData.setCustomerCode(customerCode);
                     }
                     // 购方公司名称
                     if (row.getCell(4) != null) {
-                        row.getCell(4).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(4).setCellType(CellType.STRING);
                         String buyCompanyName = row.getCell(4).getStringCellValue();
                         excelData.setBuyCompanyName(buyCompanyName);
                     }
                     // 购方公司税号
                     if (row.getCell(5) != null) {
-                        row.getCell(5).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(5).setCellType(CellType.STRING);
                         String buyCompanyNum = row.getCell(5).getStringCellValue();
                         excelData.setBuyCompanyNum(buyCompanyNum);
                     }
                     // 购方公司地址
                     if (row.getCell(6) != null) {
-                        row.getCell(6).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(6).setCellType(CellType.STRING);
                         String buyCompanyAddress = row.getCell(6).getStringCellValue();
                         excelData.setBuyCompanyAddress(buyCompanyAddress);
                     }
                     // 购方公司银行账号
                     if (row.getCell(7) != null) {
-                        row.getCell(7).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(7).setCellType(CellType.STRING);
                         String buyCompanyBankAccount = row.getCell(7).getStringCellValue();
                         excelData.setBuyCompanyBankAccount(buyCompanyBankAccount);
                     }
                     // 备注
                     if (row.getCell(8) != null) {
-                        row.getCell(8).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(8).setCellType(CellType.STRING);
                         String remark = row.getCell(7).getStringCellValue();
                         excelData.setRemark(remark);
                     }
                     // 专用发票红票通知单号
                     if (row.getCell(9) != null) {
-                        row.getCell(9).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(9).setCellType(CellType.STRING);
                         String redInvoice = row.getCell(7).getStringCellValue();
                         excelData.setRedInvoice(redInvoice);
                     }
                     // 普通发票红票对应正数发票代码
                     if (row.getCell(10) != null) {
-                        row.getCell(10).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(10).setCellType(CellType.STRING);
                         String redInvoiceCode = row.getCell(10).getStringCellValue();
                         excelData.setRedInvoiceCode(redInvoiceCode);
                     }
                     // 普通发票红票对应正数发票号码
                     if (row.getCell(11) != null) {
-                        row.getCell(11).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(11).setCellType(CellType.STRING);
                         String redInvoiceNum = row.getCell(11).getStringCellValue();
                         excelData.setRedInvoiceNum(redInvoiceNum);
                     }
                     // 开票人
                     if (row.getCell(12) != null) {
-                        row.getCell(12).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(12).setCellType(CellType.STRING);
                         String billMan = row.getCell(12).getStringCellValue();
                         excelData.setBillMan(billMan);
                     }
                     // 复核人
                     if (row.getCell(13) != null) {
-                        row.getCell(13).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(13).setCellType(CellType.STRING);
                         String checkMan = row.getCell(13).getStringCellValue();
                         excelData.setCheckMan(checkMan);
                     }
                     // 收款人
                     if (row.getCell(14) != null) {
-                        row.getCell(14).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(14).setCellType(CellType.STRING);
                         String receiptMan = row.getCell(14).getStringCellValue();
                         excelData.setReceiptMan(receiptMan);
                     }
                     // 销方银行及帐号
                     if (row.getCell(15) != null) {
-                        row.getCell(15).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(15).setCellType(CellType.STRING);
                         String destroyBankAccount = row.getCell(15).getStringCellValue();
                         excelData.setDestroyBankAccount(destroyBankAccount);
                     }
                     // 销方地址电话
                     if (row.getCell(16) != null) {
-                        row.getCell(16).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(16).setCellType(CellType.STRING);
                         String destroyAddress = row.getCell(16).getStringCellValue();
                         excelData.setDestroyAddress(destroyAddress);
                     }
                     // 商品编码
                     if (row.getCell(17) != null) {
-                        row.getCell(17).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(17).setCellType(CellType.STRING);
                         String goodsNum = row.getCell(17).getStringCellValue();
                         excelData.setGoodsNum(goodsNum);
                     }
                     // 商品名称
                     if (row.getCell(18) != null) {
-                        row.getCell(18).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(18).setCellType(CellType.STRING);
                         String goodsName = row.getCell(18).getStringCellValue();
                         excelData.setGoodsName(goodsName);
                     }
                     // 规格型号
                     if (row.getCell(19) != null) {
-                        row.getCell(19).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(19).setCellType(CellType.STRING);
                         String model = row.getCell(19).getStringCellValue();
                         excelData.setModel(model);
                     }
                     // 计量单位
                     if (row.getCell(20) != null) {
-                        row.getCell(20).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(20).setCellType(CellType.STRING);
                         String unit = row.getCell(20).getStringCellValue();
                         excelData.setUnit(unit);
                     }
                     // 数量
                     if (row.getCell(21) != null) {
-                        row.getCell(21).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(21).setCellType(CellType.STRING);
                         String num = row.getCell(21).getStringCellValue();
                         excelData.setNum(num);
                     }
                     // 金额
                     if (row.getCell(22) != null) {
-                        row.getCell(22).setCellType(Cell.CELL_TYPE_NUMERIC);
+                        row.getCell(22).setCellType(CellType.NUMERIC);
                         Double money = row.getCell(22).getNumericCellValue();
                         excelData.setMoney(money);
                     }
                     // 税率
                     if (row.getCell(23) != null) {
-                        row.getCell(23).setCellType(Cell.CELL_TYPE_STRING);
+                        row.getCell(23).setCellType(CellType.STRING);
                         String rate = row.getCell(23).getStringCellValue();
                         excelData.setRate(rate);
                     }
                     // 税额
                     if (row.getCell(24) != null) {
-                        row.getCell(24).setCellType(Cell.CELL_TYPE_NUMERIC);
+                        row.getCell(24).setCellType(CellType.NUMERIC);
                         Double rateMoney = row.getCell(24).getNumericCellValue();
                         excelData.setRateMoney(rateMoney);
                     }
                     // 折扣金额
                     if (row.getCell(25) != null) {
-                        row.getCell(25).setCellType(Cell.CELL_TYPE_NUMERIC);
+                        row.getCell(25).setCellType(CellType.NUMERIC);
                         Double discountMoney = row.getCell(25).getNumericCellValue();
                         excelData.setDiscountMoney(discountMoney);
                     }
                     // 折扣金额
                     if (row.getCell(26) != null) {
-                        row.getCell(26).setCellType(Cell.CELL_TYPE_NUMERIC);
+                        row.getCell(26).setCellType(CellType.NUMERIC);
                         Double discountRateMoney = row.getCell(26).getNumericCellValue();
                         excelData.setDiscountRateMoney(discountRateMoney);
                     }
